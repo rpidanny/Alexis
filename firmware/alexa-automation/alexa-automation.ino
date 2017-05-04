fe154a0bd0af1bd82ba577ec01635a539b069ee4
rpidanny <abhishekmaharjan1993@gmail.com>
2017-05-04 20:46:40 +0545

    implemented presisitent light state using EEPROM

--- firmware/alexa-automation/alexa-automation.ino
+++ firmware/alexa-automation/alexa-automation.ino
@@ -1,19 +1,23 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <IRremoteESP8266.h>
+#include <EEPROM.h>
 #include "fauxmoESP.h"
 #include "credentials.h"
 
 #define SERIAL_BAUDRATE                 115200
+
 #define LED                             2
 
 #define RELAYPIN1  14
 #define RELAYPIN2  13
 #define RELAYPIN3  12
+#define RELAYPIN4  15
+
 
 fauxmoESP fauxmo;
 
-IRsend irsend(4);
+IRsend irsend(16);
 
 // -----------------------------------------------------------------------------
 // Wifi
@@ -44,15 +48,33 @@ void gpioSetup(){
   pinMode(RELAYPIN1,OUTPUT);
   pinMode(RELAYPIN2,OUTPUT);
   pinMode(RELAYPIN3,OUTPUT);
+  //pinMode(RELAYPIN4,OUTPUT);
   pinMode(LED, OUTPUT);
   digitalWrite(LED, HIGH);
-  digitalWrite(RELAYPIN1, HIGH);
-  digitalWrite(RELAYPIN2, HIGH);
-  digitalWrite(RELAYPIN3, HIGH);
+  digitalWrite(RELAYPIN1, EEPROM.read(0));
+  digitalWrite(RELAYPIN2, EEPROM.read(1));
+  digitalWrite(RELAYPIN3, EEPROM.read(2));
+  //digitalWrite(RELAYPIN4, HIGH);
+
+  if(EEPROM.read(3)){
+    irsend.sendNEC(16203967, 32);
+  }else{
+    irsend.sendNEC(16236607, 32);
+  }
+  
+  for(int i=0;i<4;i++){
+    Serial.println(EEPROM.read(i),DEC);
+  }
 }
 
+void writeFlash(int addr,byte data){
+  EEPROM.write(addr, data);
+  EEPROM.commit();
+}
 void setup() {
 
+    EEPROM.begin(4);
+    
     // Init serial port and clean garbage
     Serial.begin(SERIAL_BAUDRATE);
     Serial.println();
@@ -61,14 +83,15 @@ void setup() {
     //ir setup
     irsend.begin();
 
-    irsend.sendNEC(0xF740BF, 32);
-    delay(1000);
-    irsend.sendNEC(0xF7C03F, 32);
-    // Wifi
-    wifiSetup();
+    //irsend.sendNEC(0xF740BF, 32);
+    //delay(1000);
+    //irsend.sendNEC(0xF7C03F, 32);
 
     // Setup GPIO
     gpioSetup();
+    
+    // Wifi
+    wifiSetup();
 
     // Fauxmo
     fauxmo.addDevice("movie light");
@@ -82,12 +105,16 @@ void setup() {
         String device = String(device_name);
         Serial.printf("[MAIN] Device #%d (%s) state: %s\n", device_id, device_name, state ? "ON" : "OFF");
         if(device.equals("dome light")){
+          writeFlash(0,!state);
           digitalWrite(RELAYPIN1, !state);
         }else if(device.equals("tube light")){
+          writeFlash(1,!state);
           digitalWrite(RELAYPIN2, !state);
         }else if(device.equals("bed light")){
+          writeFlash(2,!state);
           digitalWrite(RELAYPIN3, !state);
         }else if(device.equals("movie light")){
+          writeFlash(3,!state);
           digitalWrite(LED, !state);
           if(state){
             irsend.sendNEC(16236607, 32);
