65791e2b07c2fceb4c634ab645152b7189b6832a
rpidanny <abhishekmaharjan1993@gmail.com>
2017-05-06 00:10:47 +0545

    post sensor data to heroku datalogger

--- firmware/alexa-automation/alexa-automation.ino
+++ firmware/alexa-automation/alexa-automation.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <IRremoteESP8266.h>
+#include <ESP8266HTTPClient.h>
 #include <EEPROM.h>
 #include "fauxmoESP.h"
 #include "credentials.h"
@@ -18,7 +19,7 @@
 #define RELAYPIN3  12
 #define RELAYPIN4  15
 
-#define IRTX 16
+#define IRTX 5
 #define IRRX 0
 
 fauxmoESP fauxmo;
@@ -37,7 +38,6 @@ Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO
 Adafruit_MQTT_Publish humidity = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/humidity");
 Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temperature");
 Adafruit_MQTT_Publish heatindex = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/heatindex");
-
 
 // -----------------------------------------------------------------------------
 // Wifi
@@ -126,6 +126,9 @@ void pushSensorData(){
   Serial.print(" : ");
   Serial.print(hic);
   Serial.print(" -- ");
+
+  postJSON(h,t,hic);
+  
   if (! humidity.publish(h) || ! temperature.publish(t) || ! heatindex.publish(hic) ) {
     Serial.println(F("Failed"));
   } else {
@@ -133,6 +136,15 @@ void pushSensorData(){
   }
 }
 
+void postJSON(float h,float t,float hic){
+  //http client
+  HTTPClient http;
+  http.begin("http://sensor-datalogger.herokuapp.com/api/v1/sensor");
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  http.POST("humidity="+String(h)+"&temperature="+String(t)+"&heatIndex="+String(hic)+"&key=142c647df295c9b92c994db569ec23a6e75760a8a57e9bfa24fdfa7eaaf1d13e");
+  http.writeToStream(&Serial);
+  http.end();
+}
 void checkConnection(){
   if(WiFi.status() != WL_CONNECTED){
     wifiSetup();
@@ -157,8 +169,14 @@ void setup() {
     //irsend.sendNEC(0xF7C03F, 32);
 
     // Setup GPIO
-    gpioSetup();
+    //gpioSetup();
 
+    if(EEPROM.read(3)){
+      irsend.sendNEC(16203967, 32);
+    }else{
+      irsend.sendNEC(16236607, 32);
+    }
+    
     //setup DHT sensor
     dht.begin();
     
@@ -169,26 +187,14 @@ void setup() {
     MQTT_connect();
      
     // Fauxmo
-    fauxmo.addDevice("movie light");
-    fauxmo.addDevice("bed light");
-    fauxmo.addDevice("tube light");
-    fauxmo.addDevice("dome light");
-
+    fauxmo.addDevice("desk light");
+    
     // fauxmoESP 2.0.0 has changed the callback signature to add the device_id, this WARRANTY
     // it's easier to match devices to action without having to compare strings.
     fauxmo.onMessage([](unsigned char device_id, const char * device_name, bool state) {
         String device = String(device_name);
         Serial.printf("[MAIN] Device #%d (%s) state: %s\n", device_id, device_name, state ? "ON" : "OFF");
-        if(device.equals("dome light")){
-          writeFlash(0,!state);
-          digitalWrite(RELAYPIN1, !state);
-        }else if(device.equals("tube light")){
-          writeFlash(1,!state);
-          digitalWrite(RELAYPIN2, !state);
-        }else if(device.equals("bed light")){
-          writeFlash(2,!state);
-          digitalWrite(RELAYPIN3, !state);
-        }else if(device.equals("movie light")){
+        if(device.equals("desk light")){
           writeFlash(3,!state);
           //digitalWrite(LED, !state);
           if(state){
@@ -198,7 +204,6 @@ void setup() {
           }
         }
     });
-
 }
 
 void loop() {
