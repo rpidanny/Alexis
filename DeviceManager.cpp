a72229037df345dec9c078e81c0e9a4c823f0251
rpidanny <abhishekmaharjan1993@gmail.com>
2018-06-18 17:12:22 +0200

    eeprom interfacing

--- DeviceManager.cpp
+++ DeviceManager.cpp
@@ -1,6 +1,18 @@
 #include "DeviceManager.h"
 
 void DeviceManager::begin() {
+  EEPROM.begin(SIZE);
+  Serial.println(SIZE);
+  Device test = {
+    0,
+    "Wemo Switch"
+  };
+  EEPROM.put(4 * sizeof(Device), test);
+  for (uint8_t i =0; i < DEVICES * sizeof(Device); i+= sizeof(Device)) {
+    Device d;
+    EEPROM.get(i, d);
+    Serial.println(d.name);
+  }
   fauxmo.addDevice("light one");
   // fauxmo.addDevice("light two");
   // fauxmo.addDevice("light three");
@@ -17,6 +29,15 @@ void DeviceManager::begin() {
 
 void DeviceManager::handle() {
   fauxmo.handle();
+}
+
+uint8_t DeviceManager::readROM(uint8_t addr) {
+  return EEPROM.read(addr);
+}
+
+void DeviceManager::writeROM(uint8_t addr, uint8_t data) {
+  EEPROM.write(addr, data);
+  EEPROM.commit();
 }
 
 DeviceManager DM;
--- DeviceManager.h
+++ DeviceManager.h
@@ -2,6 +2,16 @@
 #define DeviceMANAGER_H
 
 #include <fauxmoESP.h>
+#include <EEPROM.h>
+
+#define DEVICES 5
+#define NAME_LENGTH 20
+#define SIZE sizeof(Device) * DEVICES // Maxing at 5 devices for now
+
+struct Device {
+  uint8_t pin;
+  char name[NAME_LENGTH];
+};
 
 class DeviceManager {
   public:
@@ -9,6 +19,8 @@ class DeviceManager {
     void handle();
   private:
     void DEBUG_DM(String msg);
+    uint8_t readROM(uint8_t addr);
+    void writeROM(uint8_t addr, uint8_t data);
 
     bool _debug = true;
     fauxmoESP fauxmo;
