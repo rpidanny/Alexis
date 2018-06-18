b360e28b15f1cb0bfc18d4ad84ab421d1a5248c0
rpidanny <abhishekmaharjan1993@gmail.com>
2018-06-18 12:53:39 +0200

    start from scratch

---
+++ alexa-node.ino
@@ -0,0 +1,28 @@
+#include <fauxmoESP.h>
+
+fauxmoESP fauxmo;
+
+void setup() {
+
+    Serial.begin(115200);
+
+    // ... connect to wifi ...
+
+    fauxmo.addDevice("light one");
+    fauxmo.addDevice("light two");
+    fauxmo.addDevice("light three");
+    fauxmo.addDevice("light four");
+    fauxmo.enable(true);
+
+    fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state) {
+        Serial.printf("[MAIN] Device #%d (%s) state: %s\n", device_id, device_name, state ? "ON" : "OFF");
+    });
+    fauxmo.onGetState([](unsigned char device_id, const char * device_name) {
+        return true; // whatever the state of the device is
+    });
+
+}
+
+void loop() {
+    fauxmo.handle();
+}
